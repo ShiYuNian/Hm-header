b007846b08e032c3b615b5f96ab9ab7d83d61a3b
白衣 <3363533398@qq.com>
2020-09-01 21:52:29 +0800

    组件配置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/base.less'
+import './styles/iconfont.css'
 
+// import Vant from 'vant'
+
+import { Button } from 'vant'
+
+// import 'vant/lib/index.css'
+// Vue.use(Vant)
+Vue.use(Button)
+// import Button from 'vant/lib/button'
+// import 'vant/lib/button/style'
 Vue.config.productionTip = false
 
 new Vue({
